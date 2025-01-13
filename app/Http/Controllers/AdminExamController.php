ec6fdd0e71f42991aff528e0b0527d1b609be552
Nguyen Duc Thang <ducthangofficial03@gmail.com>
2025-01-13 21:07:38 +0700

    abc

--- app/Http/Controllers/AdminExamController.php
+++ app/Http/Controllers/AdminExamController.php
@@ -6,8 +6,17 @@ use Illuminate\Http\Request;
 
 class AdminExamController extends Controller
 {
-    //
     function list(){
-        return view('admin.exam.list');
+        $exams = Exam::with('level')->get();
+        return view('admin.exam.list', compact('exams'));
+    }
+
+    function add(){
+        return view('admin.exam.add');
+    }
+
+    function delete ($id){
+        Exam::find($id)->delete();
+        return redirect('admin/exam/list');
     }
 }
